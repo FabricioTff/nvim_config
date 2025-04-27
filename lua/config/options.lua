689aa71bb7f9fd5eb6b3a35eb7dff7038ac298fd
fabricio <fabriciotfferreira@gmail.com>
2025-04-26 21:53:02 -0300

    my neovim config

---
+++ lua/config/options.lua
@@ -0,0 +1,50 @@
+vim.opt.expandtab = true -- convert tabs to space
+vim.opt.shiftwidth = 4 -- Amount to ident with << and >
+
+vim.opt.tabstop = 4 -- How many spaces are shown per Tab
+vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true -- Keep indentation from previous line
+
+vim.opt.number = true
+
+vim.opt.relativenumber = true
+
+vim.opt.undofile = true
+
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+vim.opt.breakindent = true
+
+vim.opt.ignorecase = true
+
+vim.opt.smartcase = true 
+
+
+-- Keep sign column on by default
+vim.opt.signcolumn = "yes"
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+-- vim.opt.listchars = {tab = "│", trail = '.', nbsp = "␣"}
+
+
+vim.opt.scrolloff = 10
+
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
