a6f4728058dad4e75aac19f189d76a8b3320f837
fabricio <fabriciotfferreira@gmail.com>
2025-04-28 07:47:45 -0300

    my nvim config

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,25 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  keys = {
+    {
+      "<leader>ff", 
+      function() require('fzf-lua').files() end, 
+      desc = "Open Fzf in project directory"
+    },
+    {
+      "<leader>fg", 
+      function() require('fzf-lua').live_grep() end, 
+      desc = "Find by grepping current working directory"
+    },
+    { 
+      "<leader>fg",
+      function() require('fzf-lua').live_grep() end,
+      desc="Find by grepping in project directory"
+    }
+  }
+}
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -8,6 +8,15 @@ return {
 			sync_install = false,
 			highlight = {enable = true},
 			indent = {enable = true},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+					node_incremental = "<Enter>",
+					scope_incremental = false,
+					node_decremental = "<Backspace>",
+				},
+			},
 		})
-		end
-	}
+	end
+}
